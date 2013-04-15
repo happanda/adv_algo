b1529284d44ed9b3b6b237b08b39c26a528a885d
Pavel Moskalevich <happy.proger@gmail.com>
2013-04-16 01:03:43 +0400

    push/pop Front/Back functions (some errors exist)

--- persistent_deque/src/Main.java
+++ persistent_deque/src/Main.java
@@ -6,11 +6,21 @@ public class Main {
             PersistentDeque<Integer> deque = new PersistentDeque<Integer>();
             final int count = 12;
             for (int i = 0; i < count; ++i) {
-                deque = deque.push(i);
+                deque = deque.pushFront(i);
             }
 
             for (int i = 0; i < count; ++i) {
-                Pair<Integer, PersistentDeque<Integer>> p = deque.pop();
+                Pair<Integer, PersistentDeque<Integer>> p = deque.popBack();
+                System.out.println(p.first);
+                deque = p.second;
+            }
+
+            for (int i = 0; i < count; ++i) {
+                deque = deque.pushBack(i);
+            }
+
+            for (int i = 0; i < count; ++i) {
+                Pair<Integer, PersistentDeque<Integer>> p = deque.popFront();
                 System.out.println(p.first);
                 deque = p.second;
             }
--- persistent_deque/src/PersistentDeque.java
+++ persistent_deque/src/PersistentDeque.java
@@ -8,27 +8,50 @@ class PersistentDeque<T> {
         return head == null && tail == null && child == null;
     }
 
-    public PersistentDeque<T> push(T value) {
+    public PersistentDeque<T> pushFront(T value) {
         if (empty())
             return new PersistentDeque<T>(value, null, null);
         if (head == null)
             return new PersistentDeque<T>(value, child, tail);
         if (child == null)
             child = new PersistentDeque<Pair<T, T>>();
-        return new PersistentDeque<T>(null, child.push(new Pair<T, T>(value, head)), tail);
+        return new PersistentDeque<T>(null, child.pushFront(new Pair<T, T>(value, head)), tail);
     }
 
-    public Pair<T, PersistentDeque<T>> pop() throws Exception {
+    public Pair<T, PersistentDeque<T>> popFront() throws Exception {
         if (empty())
             throw new Exception("Pop from empty deque");
         if (head != null)
             return new Pair<T, PersistentDeque<T>>(head, new PersistentDeque<T>(null, child, tail));
         if (child != null)
         {
-            Pair<Pair<T, T>, PersistentDeque<Pair<T, T>>> res = child.pop();
+            Pair<Pair<T, T>, PersistentDeque<Pair<T, T>>> res = child.popFront();
             return new Pair<T, PersistentDeque<T>>(res.first.first, new PersistentDeque<T>(res.first.second, res.second, tail));
         }
         return new Pair<T, PersistentDeque<T>>(tail, new PersistentDeque<T>());
+    }
+
+    public PersistentDeque<T> pushBack(T value) {
+        if (empty())
+            return new PersistentDeque<T>(null, null, value);
+        if (tail == null)
+            return new PersistentDeque<T>(head, child, value);
+        if (child == null)
+            child = new PersistentDeque<Pair<T, T>>();
+        return new PersistentDeque<T>(head, child.pushBack(new Pair<T, T>(tail, value)), null);
+    }
+
+    public Pair<T, PersistentDeque<T>> popBack() throws Exception {
+        if (empty())
+            throw new Exception("Pop from empty deque");
+        if (tail != null)
+            return new Pair<T, PersistentDeque<T>>(tail, new PersistentDeque<T>(head, child, null));
+        if (child != null)
+        {
+            Pair<Pair<T, T>, PersistentDeque<Pair<T, T>>> res = child.popBack();
+            return new Pair<T, PersistentDeque<T>>(res.first.second, new PersistentDeque<T>(head, res.second, res.first.first));
+        }
+        return new Pair<T, PersistentDeque<T>>(head, new PersistentDeque<T>());
     }
 
     private PersistentDeque(T head, PersistentDeque<Pair<T, T>> child, T tail) {
