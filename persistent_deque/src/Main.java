d94de504dab9a445c491d2112393474aa9ec6efa
Pavel Moskalevich <happy.panda.code@gmail.com>
2013-04-21 14:51:48 +0400

    Removed unnecessary try-catch.

--- persistent_deque/src/Main.java
+++ persistent_deque/src/Main.java
@@ -8,19 +8,13 @@ public class Main {
     }
 
     private static void RunTests() {
-        try
-        {
-            DequeTest dt = new DequeTest();
-            dt.TestPushPopFront();
-            dt.TestPushPopBack();
-            dt.TestPushFrontPopBack();
-            dt.TestPushBackPopFront();
-            dt.TestFillEmpty();
-            dt.TestRandomPushPop();
-        }
-        catch (Exception ex) {
-            System.err.println(ex.getMessage());
-        }
+        DequeTest dt = new DequeTest();
+        dt.TestPushPopFront();
+        dt.TestPushPopBack();
+        dt.TestPushFrontPopBack();
+        dt.TestPushBackPopFront();
+        dt.TestFillEmpty();
+        dt.TestRandomPushPop();
     }
 }
 
